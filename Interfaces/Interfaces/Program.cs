9421513afcb6571a9c2078d9352e20cffc4b13b4
ewurafuaa <equansah058@st.ug.edu.gh>
2025-08-04 21:57:17 +0000

    Interfaces

---
+++ Interfaces/Interfaces/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interfaces
+{
+    interface IMovable
+    {
+        void Move();
+    }
+
+    class Car : IMovable
+    {
+        public void Move()
+        {
+            Console.WriteLine("Car is moving");
+        }
+    }
+
+    class Bicycle : IMovable
+    {
+        public void Move()
+        {
+            Console.WriteLine("Bicycle is moving");
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Car myCar = new Car();
+            Bicycle myBike = new Bicycle();
+
+            myCar.Move();
+            myBike.Move();
+        }
+    }
+}
