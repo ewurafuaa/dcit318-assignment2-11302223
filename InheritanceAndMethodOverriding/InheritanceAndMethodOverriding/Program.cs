13c4d4374f96391ec6865a8f9693a9cc9f4298cf
ewurafuaa <equansah058@st.ug.edu.gh>
2025-08-04 21:53:27 +0000

    Inheritance and Method Overriding

---
+++ InheritanceAndMethodOverriding/InheritanceAndMethodOverriding/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InheritanceAndMethodOverriding {
+    class Animal{
+        public virtual void MakeSound(){
+            Console.WriteLine("Some generic sound");
+        }
+    }
+
+    class Dog : Animal{
+        public override void MakeSound(){
+            Console.WriteLine("Bark");
+        }
+    }
+
+    class Cat : Animal{
+        public override void MakeSound(){
+            Console.WriteLine("Meow");
+        }
+    }
+
+    class Program{
+        static void Main(string[] args){
+            Animal a = new Animal();
+            Dog d = new Dog();
+            Cat c = new Cat();
+
+            a.MakeSound();
+            d.MakeSound();
+            c.MakeSound();
+
+        }
+    }
+}
